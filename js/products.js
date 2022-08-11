76d7baf673b442da35e007c3a2905065242f3918
Mario Ourthe - Cabalé <mourthecabalediaz@gmail.com>
2022-08-11 18:01:31 -0300

    Cambio de index y acceso a listado Productos_Autos Entrega_1

---
+++ js/products.js
@@ -0,0 +1,42 @@
+let PRODRUCTS_AUTOS = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+
+
+function showProductsList(){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentProductsArray.length; i++){
+        let products = currentProductsArray[i];
+        htmlContentToAppend += `
+                <div class="row">
+                    <div class="col-3">
+                        <img src="${products.image}" alt="${products.description}" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${products.name}+"-"${products.currency}+" "+${products.cost}</h4>
+                            <small class="text-muted">${products.soldCount} artículos</small>
+                        </div>
+                        <p class="mb-1">${products.description}</p>
+                    </div>
+                </div>
+            </div>
+            `
+        }
+
+        document.getElementById("Productos-autos").innerHTML += htmlContentToAppend;
+    }
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODRUCTS_AUTOS).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            currentProductsArray = resultObj.data.products
+            console.log(currentProductsArray.length)
+            showProductsList()
+        }
+    });
+})
