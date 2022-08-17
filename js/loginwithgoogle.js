105ac7d65695a7148cb287901b6af164ec502da4
Mario Ourthe - Cabalé <mourthecabalediaz@gmail.com>
2022-08-17 01:15:31 -0300

    Autenticacion google 1.1

--- js/loginwithgoogle.js
+++ js/loginwithgoogle.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-import { initializeApp } from 'firebase/app';
+/* import { initializeApp } from 'firebase/app'; */
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
